56a22a5449d5cb55f7c53ccf6eda60d390637bf9
Kim Sangyeon <dho113@naver.com>
2019-11-24 22:50:17 +0900

    Function Components

--- src/js/component/Square.js
+++ src/js/component/Square.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button 
-        className='square'
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+const Square = (props) => {
+  return (
+    <button 
+      className='square'
+      onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  );
 }
 
 export default Square;
