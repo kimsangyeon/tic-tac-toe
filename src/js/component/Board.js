517e5a21bb359da9823602f437bd564cd818ef12
Kim Sangyeon <dho113@naver.com>
2019-11-24 22:56:56 +0900

    Taking Turns

--- src/js/component/Board.js
+++ src/js/component/Board.js
@@ -6,13 +6,17 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -25,7 +29,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next Player: X';
+    const status = `Next Player: ${this.state.xIsNext ? 'X' : 'O'}`;
 
     return (
       <div>
